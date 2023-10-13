71cbf86517681f447e20cbd2707fef7bcbf8a44c
turrud <fatur14022@gmail.com>
2023-10-14 02:28:26 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [OrderController::class, 'index']);
 Route::post('/checkout', [OrderController::class, 'store']);
-Route::post('/midtrans-callback', [OrderController::class, 'callback']);
