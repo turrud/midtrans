dc3ad90ca127004389fa034159758e63b891f443
turrud <fatur14022@gmail.com>
2024-12-28 14:01:25 +0700

    callback

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -28,7 +28,7 @@ class OrderController extends Controller
         $serverKey = config('midtrans.server_key');
         $hashed = hash("sha512", $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
         if ($hashed == $request->signature_key) {
-            if ($request->transaction_status == 'capture' or $request->transaction_status == 'settlement') {
+            if ($request->transaction_status == 'capture') {
                 $order = Order::find($request->order_id);
                 $order->update([
                     'status' => 'paid',
